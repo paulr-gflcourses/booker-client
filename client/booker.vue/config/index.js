12e684def5bdf4b9f17cd8cc913af57189cce6df
student <student@GFL-1465.ad.geeksforless.net>
2018-10-01 21:36:00 +0300

    fixing path

--- client/booker.vue/config/index.js
+++ client/booker.vue/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/~user12/booker-client/',
     proxyTable: {},
 
     // Various Dev Server settings
