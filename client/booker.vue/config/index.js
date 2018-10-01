6f8f7f87e3e8fc7a594516efc48ac6e533605a01
student <student@GFL-1465.ad.geeksforless.net>
2018-10-01 21:44:36 +0300

    fixing

--- client/booker.vue/config/index.js
+++ client/booker.vue/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/~user12/booker-client',
 
     /**
      * Source Maps
